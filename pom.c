ca89b8bd92b7c808b766f144ea30ce9ce05be266
greg <greg@fittedcloud.com>
2018-03-27 11:22:25 -0400

    Unfortunately, removed a bit shift, but made things more readable, so i guess it's a wash

--- pom.c
+++ pom.c
@@ -8,15 +8,14 @@ MODULE_LICENSE("Dual BSD/GPL");
 #define MINS_TO_MS(a) ((a) * 60 * 1000)
 
 /* sleep 25 minutes +/- 5 minutes before kernel panic */
-#define POM_DEFAULT_START (25)
+#define POM_DEFAULT_START (25-5)
 #define POM_DEFAULT_WINDOW (10)
 
 static int pom_init(void)
 {
     int mins;
     get_random_bytes(&mins, sizeof(mins));
-    mins = (mins % POM_DEFAULT_WINDOW) - (POM_DEFAULT_WINDOW >> 1);
-    mins += POM_DEFAULT_START;
+    mins = (mins % POM_DEFAULT_WINDOW) + POM_DEFAULT_START;
 
     msleep(MINS_TO_MS(mins));
     panic("Take a break already!");
@@ -25,7 +24,9 @@ static int pom_init(void)
 
 static void pom_exit(void)
 {
-    printk(KERN_ALERT "Exiting\n");
+    printk(KERN_ALERT "If you put in the effort to manually remove "
+                      "this module, then you're clearly not trying "
+                      "to optimize your productivity!\n");
 }
 
 module_init(pom_init);
